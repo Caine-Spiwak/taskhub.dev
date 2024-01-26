25582dbe4de3464fe03035e126b0eabdb27a6129
Caine <143353083+Caine-Spiwak@users.noreply.github.com>
2024-01-26 13:40:44 -0800

    Add Project Section UI and API functionality

--- frontend/src/screens/Dashboard.jsx
+++ frontend/src/screens/Dashboard.jsx
@@ -1,8 +1,23 @@
-
+import './Dashboard.css'
+import ProjectSection from '../features/projects/ProjectSection'
+// import { Link } from 'react-router-dom';
+// import TaskSection from '../features/tasks/components/TaskSection.jsx';
 
 const Dashboard = () => {
+
   return (
-    <div>Dashboard</div>
+    <div className="window">
+      <div className="sidebar">
+        <ProjectSection />
+        <div className='profile-section'>
+          <hr />
+          {/* <Link className='profile-link' to='/profile'>Profile</Link> */}
+        </div>
+      </div>  
+      <div className="content">
+        {/* <TaskSection /> */}
+      </div>
+    </div>
   )
 }
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from "./slices/apiSlice";
-import authSliceReducer from './slices/authSlice'
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './slices/authSlice'
+import activeProjectReducer from './slices/activeProjectSlice'
+import { apiSlice } from './slices/apiSlice'
 
 const store = configureStore({
   reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    auth: authSliceReducer,
+      auth: authReducer,
+      activeProject: activeProjectReducer,
+			[apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true
